c1168dad40102a5b7b296d0e9485b3eaca412b8b
Bill Moore <bill@nzmoore.com>
2016-10-12 18:00:56 -0400

    After Hello World on Node.js

--- hello_world/app.js
+++ hello_world/app.js
@@ -1,1 +1,10 @@
-console.log("Hi There");
+var http = require('http');
+
+var server =http.createServer(function (req, resp) {
+resp.writeHead(200, {"Content-Type": "text/plain"});
+resp.end("Hi there suckers\nWhats up");
+});
+
+server.listen(8080)
+
+console.log("Node http server listening on port 8080");
