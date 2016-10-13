1bdc714213635ebc22390aaa6e1bbdb19eb987e3
Bill Moore <bill@nzmoore.com>
2016-10-13 15:44:02 -0400

    After all together now

--- hello_world/app.js
+++ hello_world/app.js
@@ -1,20 +1,35 @@
 var express = require('express'),
     app = express(),
+    MongoClient = require('mongodb').MongoClient,
+    assert = require('assert'),
     engines = require('consolidate');
     
     app.engine("html", engines.nunjucks);
     app.set('view engine', 'html');
     app.set('views', __dirname + '/views');
+    
+    MongoClient.connect('mongodb://localhost:27017/video', function(err, db) {
+    
+      assert.equal(null, err);
+      console.log('db connected');
 
-app.get("/", function(req, res) {
-    res.render('hello', {'name': 'Templates'} );
-});
+       app.get("/", function(req, res) {
+       res.render('hello', {'name': 'Templates'} );
+       });
+       
+       app.get("/movies", function(req, res) {
+          db.collection('movies').find({}).toArray(function(err, docs) {
+              res.render('movies', {'movies': docs } );
+          });
+       
+       });
+       
+       app.use(function(req,res) {
+       res.sendStatus(404);
+       });
 
-app.use(function(req,res) {
-   res.sendStatus(404);
-});
-
-var server = app.listen(3030, function() {
-   var port = server.address().port;
-   console.log("Express server listening on port %s", port);
-}); 
+       var server = app.listen(3030, function() {
+       var port = server.address().port;
+       console.log("Express server listening on port %s", port);
+       }); 
+     });  
