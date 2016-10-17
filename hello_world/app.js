ecd7dd14aaf55ee1e82abc282c1d11ec9af95c7d
Bill Moore <bill@nzmoore.com>
2016-10-16 21:33:26 -0400

    Week one done

--- hello_world/app.js
+++ hello_world/app.js
@@ -2,42 +2,99 @@ var express = require('express'),
     app = express(),
     MongoClient = require('mongodb').MongoClient,
     assert = require('assert'),
+    bodyParser = require('body-parser'),
     engines = require('consolidate');
     
     app.engine("html", engines.nunjucks);
     app.set('view engine', 'html');
     app.set('views', __dirname + '/views');
+    app.use(bodyParser.urlencoded({ extended: true })); 
+    
+    // Handler for internal server errors
+    function errorHandler(err, req, res, next) {
+       console.error(err.message);
+       console.error(err.stack);
+    res.status(500).render('error-template', { error: err });
+    }
+    
     
     MongoClient.connect('mongodb://localhost:27017/video', function(err, db) {
     
       assert.equal(null, err);
       console.log('db connected');
 
-       app.get("/", function(req, res) {
+       app.get("/", function(req, res, next) {
        res.render('hello', {'name': 'Templates'} );
        });
        
-       app.get("/movies", function(req, res) {
+       app.get("/testErrorTemplate", function(req, res, next) {
+        next("Some other error text");
+       });
+       
+       app.get("/movies", function(req, res, next) {
           db.collection('movies').find({}).toArray(function(err, docs) {
               res.render('movies', {'movies': docs } );
           });
        
        });
        
-       app.get("/:showvars", function(req, res) {
-         var showvars = req.params.showvars;
+       app.get("/addMovies", function(req, res, next) {
+              var messageText = " "; 
+              res.render('addMovies', { "message": messageText } );
+        
+       
+       });
+       
+       app.post("/addMovie", function(req, res, next) {
+              
+              var messageText = " ";
+              var title = req.body.title; 
+              var year = req.body.year;
+              var id = req.body.imdb;
+              var addData = true
+              
+              if (!title) {
+     			   messageText = "Enter a title! ";
+     			   addData = false;
+   			  };
+            
+              if (!year) {
+     			   messageText = messageText + "Enter a year! ";
+     			   addData = false;
+   			  };
+   			  
+   			  if (!id) {
+     			   messageText = messageText + "Enter an IMDB ID! ";
+     			   addData = false;
+   			  };
+               
+              if (addData) {
+                messageText = "Movie added " + title + " " + year + " " + id ;   
+                db.collection('movies').insertOne({"title": title, "year": year, "imdb": id});
+              };    
+              
+              res.render('addMovies', { "message": messageText } );
+       });
+       
+       app.get("/showvars", function(req, res, next) {
+         var showvars = "Junk";
          var reqVar = req.query;
+         // console.log("Params is " req.params);
          
          for (var prop in reqVar) {
-            console.log("reqVar." + prop + " = " + reqVar[prop]);
+            console.log("This reqVar." + prop + " = " + reqVar[prop]);
          }
          res.render('showvars', {'name': showvars,'params': reqVar});
           
        });
        
-       app.use(function(req,res) {
-       res.sendStatus(404);
+       app.get("/:allothers", function(req, res, next) {
+          var aRoute = req.params.allothers;
+          next("Route " + aRoute +  " not found");
        });
+       
+       app.use(errorHandler);
+       
 
        var server = app.listen(3030, function() {
        var port = server.address().port;
