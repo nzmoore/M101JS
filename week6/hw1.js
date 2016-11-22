6bc92335ee4d195fab70ea23eceb85f3cb9cd298
Bill Moore <bill@nzmoore.com>
2016-11-21 23:50:58 -0500

    step 2

--- week6/hw1.js
+++ week6/hw1.js
@@ -1,12 +1,11 @@
 db.companies.aggregate( [
-    { $match: { "relationships.person": { $ne: null } } },
+    { $match: { "relationships.person.permalink": "josh-stein" } },
     { $project: { name: 1, relationships: 1, _id: 0 } },
     { $unwind: "$relationships" },
     { $group: {
         _id: "$relationships.person",
-        companies: { $addToSet: "$name" },
-        count: { $sum: 1 }
-    } },
-    { $sort: { count: -1 } }
+        companies: { $addToSet: "$name" }    
+    } }, 
+    { $match: { "_id.permalink": "josh-stein" } }
 ] )
 
